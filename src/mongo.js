c6a2284a30233bfdc97765f570364047baa7be9b
atifibrahim7 <bigdream382@gmail.com>
2024-02-11 13:20:01 +0500

    form

--- src/mongo.js
+++ src/mongo.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/LoginFormPractice")
+mongoose.connect("mongodb+srv://bigdream382:Abbasi123$@cluster0.dvzdjwf.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
     console.log('mongoose connected');
 })
@@ -8,17 +8,168 @@ mongoose.connect("mongodb://localhost:27017/LoginFormPractice")
     console.log('failed');
 })
 
-const logInSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+
+// Schema for the taxpayer object
+const taxpayerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    password:{
+    SSN: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    occupation: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String
+    },
+    email: {
+        type: String
+    }
+});
+// Schema for the spouse object
+const spouseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    SSN: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    occupation: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String
+    },
+    email: {
+        type: String
+    }
+});
+// Schema for the address object
+const addressSchema = new mongoose.Schema({
+    streetNo: {
+        type: String,
+    },
+    city: {
+        type: String,
+    },
+    zip: {
+        type: String,
+    },
+    county: {
+        type: String
+    },
+    state: {
+        type: String,
+    },
+    homePhone: {
+        type: String
+    }
+});
+const additionalQuestionsSchema = new mongoose.Schema({
+    question1: {
+        type: String,
+    },
+    question2a: 
+    {
+        type: String,
+    },
+    question2b: {
+        type: String,
+    },
+    question3: {
+        type: String,
+    },
+    question4: {
+        type: String,
+    },
+    question5: {
+        type: String,
+    }
+});
+const dependentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    relationship: {
+        type: String,
+    },
+    dob: {
+        type: Date,
+    },
+    ssn: {
+        type: String
+    },
+    residentStatus: {
+        type: String
+    },
+    monthsLivedWithYou: {
+        type: Number
+    },
+    disabled: {
+        type: Boolean
+    }
+});
+
+const bankDetailsSchema = new mongoose.Schema({
+    bankName: {
+        type: String,
+    },
+    accountType: {
+        type: String,
+    },
+    routingNumber: {
+        type: String,
+    },
+    accountNumber: {
+        type: String,
+    },
+    hasForeignAccounts: {
+        type: Boolean,
+    }
+});
+// incomplete schema for the form object
+// we need to add the rest of the fields
+const formSchema=new mongoose.Schema({
+    taxyear:{
         type:String,
-        required:true
+    },
+    taxpayer:{
+        type:taxpayerSchema,
+    },
+    spouse:{
+        type:spouseSchema,
+    },
+    address:{
+        type:addressSchema,
+    } ,
+    additonal:{
+        type:additionalQuestionsSchema
+    },
+    dependents: [dependentSchema] // Array of dependent objects
+    ,
+    bankdetail :{
+        type:bankDetailsSchema,
     }
 })
 
-const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
 
-module.exports=LogInCollection
+
+
+
+const FormCollection=new mongoose.model('FormCollection',formSchema)
+module.exports=FormCollection
